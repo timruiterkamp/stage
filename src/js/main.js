c30083dd76a0e1063db6453e6e274160ae71bd80
Tim Ruiterkamp <timruiterkamp@gmail.com>
2016-06-10 16:55:09 +0200

    Added menu functionality and intro image

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
 // Main application logic
+window.$ = require('jQuery');
+
+$('.l-nav-placeholder').click(function() {
+ $('.l-primary-nav').toggleClass('nav-active');
+});
+
+$('.l-primary-nav__cross').click(function() {
+ $('.l-primary-nav').removeClass('nav-active');
+});
