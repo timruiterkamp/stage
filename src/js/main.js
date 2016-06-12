51f99667460128399288d6d00419aa060e4090ac
Tim Ruiterkamp <timruiterkamp@gmail.com>
2016-06-12 21:22:51 +0200

    Added homepage styling and functionality

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,21 @@ $('.l-nav-placeholder').click(function() {
 $('.l-primary-nav__cross').click(function() {
  $('.l-primary-nav').removeClass('nav-active');
 });
+
+$('.intro__scroll').click(function() {
+    $('html,body').animate({
+     scrollTop: $("#introduction").offset().top
+  });
+});
+
+$(document).keyup(function(e) {
+     if (e.keyCode == 27) { // escape key maps to keycode `27`
+       console.log('gedrukt');
+      $('.l-primary-nav').toggleClass('nav-active');
+    }
+    else if (e.keyCode == 40) {
+      $('html,body').animate({
+       scrollTop: $("#introduction").offset().top
+      });
+    }
+});
