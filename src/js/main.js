1d08131c08bfe3f523006e219b34d55e655cd57d
Tim Ruiterkamp <timruiterkamp@gmail.com>
2016-06-15 09:34:02 +0200

    Added images and bedrijfsinfo

--- src/js/main.js
+++ src/js/main.js
@@ -30,9 +30,9 @@ $(document).keyup(function(e) {
         {
           y: 200,
           opacity: 0,
-          delay: 0.25,
+          delay: 0.2,
         },
-          0.5
+          0.2
         );
     }
     else if (e.keyCode == 40) {
@@ -68,6 +68,36 @@ $(function() {
   .setTween(tween1)
   .addTo(controller);
 
+  var tween2 = TweenMax.staggerFrom('.company__vision', 2,
+    {
+      y: 200,
+    },
+      0.2
+    );
+
+  var scene = new ScrollMagic.Scene({
+    triggerElement: '.company',
+    duration: 900,
+    offset: -300,
+  })
+  .setTween(tween2)
+  .addTo(controller);
+
+  var tween2 = TweenMax.staggerFrom(['img.left', 'img.right'], 2,
+    {
+      y: 600,
+    },
+      0.2
+    );
+
+  var scene = new ScrollMagic.Scene({
+    triggerElement: '.company__text',
+    duration: 900,
+    offset: -300,
+  })
+  .setTween(tween2)
+  .addTo(controller);
+
   var content_right = $(".taskone_right"),
       content_left = $(".taskone_left");
 
@@ -75,8 +105,6 @@ $(function() {
 
   tl.from(content_right, 0.5, {left:-300, opacity:0});
   tl.from(content_left, 0.5, {right:-100, opacity:0});
-
-
 
   var scene = new ScrollMagic.Scene({
     triggerElement: '.task',
